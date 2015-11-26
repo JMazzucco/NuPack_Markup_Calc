912d71ea20cc5caebfca6cb5056ef5d5d111b683
John Mazzucco <John.Mazzucco@gmail.com>
2015-11-26 17:57:52 -0500

    Prompt user for total number of people needed to for the job and pass test if any value is returned

--- markup_calc.rb
+++ markup_calc.rb
@@ -1,5 +1,11 @@
 def base_price_input
 	puts "Enter initial base price"
-	base_price = gets
+	base_price = gets.chomp
+	base_price
+end
+
+def people_needed_input
+	puts "Enter the number of people needed to work on the job"
+	base_price = gets.chomp
 	base_price
 end
--- test_markup_calc.rb
+++ test_markup_calc.rb
@@ -6,4 +6,8 @@ class TestMarkupCalc < Minitest::Test
 	def test_base_price_input
 		assert(base_price_input)
 	end
+
+	def test_people_needed_input
+		assert(people_needed_input)
+	end
 end
