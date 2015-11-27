4491d6ecc40f55e7dabe45bc5a95a67f381c18a1
John Mazzucco <John.Mazzucco@gmail.com>
2015-11-27 16:23:53 -0500

    Edit get_action comment and fix indentation

--- test_markup_calc.rb
+++ test_markup_calc.rb
@@ -3,44 +3,40 @@ require 'minitest/autorun'
 
 class TestMarkupCalc < Minitest::Test
 
-	#set a value to prevent input prompt
+  #set a value to prevent additional input prompts when running tests
   def get_action
     1
- 	end
+  end
 
- 	def user_prompt(prompt)
- 		"test"
- 	end
+  def test_amount_input_for_float
+    assert_kind_of(Float, amount_input("base"))
+  end
 
-	def test_amount_input_for_float
-		assert_kind_of(Float, amount_input("base"))
-	end
+  def test_amount_input_for_integer
+    assert_kind_of(Integer, amount_input("material"))
+  end
 
-	def test_amount_input_for_integer
-		assert_kind_of(Integer, amount_input("material"))
-	end
+  def test_flat_markup
+    assert_equal(1050, flat_markup(1000))
+  end
 
-	def test_flat_markup
-		assert_equal(1050, flat_markup(1000))
-	end
+  def test_num_workers_markup
+    assert_equal(0.06, num_workers_markup(5))
+  end
 
-	def test_num_workers_markup
-		assert_equal(0.06, num_workers_markup(5))
-	end
+  def test_food_material_markup
+    assert_equal(0.13, prod_material_markup(2))
+  end
 
-	def test_food_material_markup
-		assert_equal(0.13, prod_material_markup(2))
-	end
+  def test_electronics_material_markup
+    assert_equal(0.02, prod_material_markup(3))
+  end
 
-	def test_electronics_material_markup
-		assert_equal(0.02, prod_material_markup(3))
-	end
+  def test_calc_total
+    assert_equal(1591.58, calc_total(1364.99, 0.036, 0.13))
+  end
 
-	def test_calc_total
-		assert_equal(1591.58, calc_total(1364.99, 0.036, 0.13))
-	end
-
-	def test_format_currency
-		assert_equal("2501.50", format_currency(2501.5))
-	end
+  def test_format_currency
+    assert_equal("2501.50", format_currency(2501.5))
+  end
 end
