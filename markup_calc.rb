9432f504cbb3fe9366903c2c25e41cb1f4216e5e
John Mazzucco <John.Mazzucco@gmail.com>
2015-11-27 15:48:34 -0500

    Fixing code indentation in Sublime as it appears differently in Github

--- markup_calc.rb
+++ markup_calc.rb
@@ -1,6 +1,6 @@
 #get user input
 def get_action
-	gets.chomp
+  gets.chomp
 end
 
 #convert string to an integer or float and check that it is greater than 0. If "material" is passed as the input parameter, check that it is between 1 and 4
@@ -8,57 +8,57 @@ def amount_input(input)
   amount = get_action
 
   if input == "base"
-		amount = amount.to_f.round(2)
+    amount = amount.to_f.round(2)
   elsif input == "workers" || input == "material"
-		amount = amount.to_i
+    amount = amount.to_i
   end
 
-	#prompt user to enter a valid number
-	if amount <= 0 || (!amount.between?(1,4) && input == "material")
-		puts "Please enter a valid number"
-		amount = amount_input(input)
-	end
+  #prompt user to enter a valid number
+  if amount <= 0 || (!amount.between?(1,4) && input == "material")
+    puts "Please enter a valid number"
+    amount = amount_input(input)
+  end
 
-	amount
+  amount
 end
 
 #calculate total percentage based on number of workers needed for the job
 def num_workers_markup(num_workers)
-	0.012 * num_workers
+  0.012 * num_workers
 end
 
 #return material markup percentage based on input
 def prod_material_markup(material_type)
-	if material_type == 1
-	 	material_markup = 0.075
-	elsif material_type == 2
-		material_markup = 0.13
-	elsif material_type == 3
-		material_markup = 0.02
-	else
-		material_markup = 0
-	end
+  if material_type == 1
+    material_markup = 0.075
+  elsif material_type == 2
+    material_markup = 0.13
+  elsif material_type == 3
+    material_markup = 0.02
+  else
+    material_markup = 0
+  end
 end
 
 #calculate subtotal price
 def flat_markup(base_price)
-	base_price *= 1.05
+  base_price *= 1.05
 end
 
 #calculate total price
 def calc_total(base_plus_flat, workers_markup, material_markup)
-	total = base_plus_flat + (base_plus_flat * workers_markup) + (base_plus_flat *material_markup)
+  total = base_plus_flat + (base_plus_flat * workers_markup) + (base_plus_flat *material_markup)
   total.round(2)
 end
 
 #append total price with a "0" if the price ends at the tenth decimal instead of the hundredth
 def format_currency(price)
-	if (price * 100) % 10 == 0
-		price = price.to_s + "0"
-	else
-		price = price.to_s
-	end
-	price
+  if (price * 100) % 10 == 0
+    price = price.to_s + "0"
+  else
+    price = price.to_s
+  end
+  price
 end
 
 #Prompt user for input and assign returned values
@@ -80,5 +80,5 @@ total_price = calc_total(base_plus_flat, workers_markup, material_markup)
 formatted_total = format_currency(total_price)
 
 #output total price
-puts "------------------"
+puts "-----------------"
 puts "Total: $" + formatted_total
